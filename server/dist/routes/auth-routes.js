36191a905c1a3fbfb6993dad1df9a5dd44beeb6f
Star <snabajja@gmail.com>
2025-02-02 15:49:20 -0500

    Build: modify auth-routes.ts

--- server/dist/routes/auth-routes.js
+++ server/dist/routes/auth-routes.js
@@ -4,31 +4,18 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 export const login = async (req, res) => {
     // TODO: If the user exists and the password is correct, return a JWT token
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ where: { username } });
-        if (!user) {
-            res.status(401).json({ message: 'Invalid Username or Password' });
-            return;
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            res.status(401).json({ message: 'Invalid Username or Password' });
-            return;
-        }
-        const secretKey = process.env.JWT_SECRET_KEY;
-        if (!secretKey) {
-            throw new Error('JWT secret key is not set');
-        }
-        const token = jwt.sign({ username: user.username, id: user.id }, secretKey, {
-            expiresIn: '1h',
-        });
-        res.json({ token });
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid Username or Password' });
     }
-    catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).json({ message: 'An error occurred. Please try again later.' });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid Username or Password' });
     }
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '1h', });
+    return res.json({ token });
 };
 const router = Router();
 // POST /login - Login a user
