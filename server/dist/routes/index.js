1b4d4d98a90d40ab6da3064e890fe250a88e9d80
Star <snabajja@gmail.com>
2025-02-04 15:20:06 -0500

    refactor: change file path

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
-import path from 'path';
+//mport path from 'path';
 const router = Router();
 router.use('/auth', authRoutes);
 router.use('/api', authenticateToken, apiRoutes);
-router.get('/', (_, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
+//router.get('/', (_, res) => {
+//res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+//});
+router.get('/', router);
 export default router;
