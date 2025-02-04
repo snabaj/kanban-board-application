e157ce88852eae4e32a7d090efccd1b762c408af
Star <snabajja@gmail.com>
2025-02-04 13:14:05 -0500

    refactor: change routes in routes/index.ts

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
-import { getAllUsers } from '../controllers/user-controller.js';
+//import { getAllUsers } from '../controllers/user-controller.js';
 const router = Router();
 router.use('/auth', authRoutes);
 router.use('/api', authenticateToken, apiRoutes);
-router.get('/', getAllUsers);
+router.get('/', (req, res) => {
+    res.send('Welcome to the Express Server');
+});
 export default router;
