003ea3816303a20eb7429ad9363eb36469a5a313
Star <snabajja@gmail.com>
2025-02-23 22:41:48 -0500

    Add UserSignup interface for user registration

--- client/src/interfaces/UserLogin.tsx
+++ client/src/interfaces/UserLogin.tsx
@@ -2,3 +2,9 @@ export interface UserLogin {
   username: string | null;
   password: string | null;
 }
+
+export interface UserSignup {
+  username: string | null;
+  password: string | null;
+  confirmPassword: string | null;
+}
