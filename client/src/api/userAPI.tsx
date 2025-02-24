4b5735139d0f4374b1768395e08fdcf22db4f1ed
Star <snabajja@gmail.com>
2025-02-23 22:20:12 -0500

    Refactor userAPI.tsx to add createUser function

--- client/src/api/userAPI.tsx
+++ client/src/api/userAPI.tsx
@@ -22,4 +22,28 @@ const retrieveUsers = async () => {
   }
 }
 
-export { retrieveUsers };
+const createUser = async (user: any) => {
+  try {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Auth.getToken()}`
+      },
+      body: JSON.stringify(user)
+    });
+    const data = await response.json();
+
+    if(!response.ok) {
+      throw new Error('invalid user API response, check network tab!');
+    }
+
+    return data;
+
+  } catch (err) { 
+    console.log('Error from data retrieval:', err);
+    return [];
+  }
+}
+
+export { retrieveUsers, createUser };
