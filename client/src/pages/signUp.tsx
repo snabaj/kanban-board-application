0f81ae5fb5184752e2becf731473f059150a3173
Star <snabajja@gmail.com>
2025-02-23 23:58:57 -0500

    Refactor signUp.tsx to include confirmPassword field in signup form

--- client/src/pages/signUp.tsx
+++ client/src/pages/signUp.tsx
@@ -1,19 +1,21 @@
 import { useState, FormEvent, ChangeEvent } from "react";
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 import { createUser } from "../api/userAPI";
 
 const SignUp = () => {
   const [signUpData, setSignUpData] = useState({
-    username: '',
-    password: '',
-    confirmPassword: ''
+    username: "",
+    password: "",
+    confirmPassword: "",
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setSignUpData({
       ...signUpData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -21,7 +23,7 @@ const SignUp = () => {
     e.preventDefault();
 
     if (signUpData.password !== signUpData.confirmPassword) {
-      console.error('Passwords do not match');
+      console.error("Passwords do not match");
       return;
     }
 
@@ -32,42 +34,42 @@ const SignUp = () => {
       });
       Auth.login(data.token);
     } catch (err) {
-      console.error('Failed to sign up', err);
+      console.error("Failed to sign up", err);
     }
   };
 
   return (
-    <div className='container'>
-      <form className='form' onSubmit={handleSubmit}>
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
 
         <label>Username</label>
-        <input 
-          type='text'
-          name='username'
+        <input
+          type="text"
+          name="username"
           value={signUpData.username}
           onChange={handleChange}
         />
-        
+
         <label>Password</label>
-        <input 
-          type='password'
-          name='password'
+        <input
+          type="password"
+          name="password"
           value={signUpData.password}
           onChange={handleChange}
         />
-        
+
         <label>Confirm Password</label>
-        <input 
-          type='password'
-          name='confirmPassword'
+        <input
+          type="password"
+          name="confirmPassword"
           value={signUpData.confirmPassword}
           onChange={handleChange}
         />
-        <button type='submit'>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
     </div>
-  )
+  );
 };
 
 export default SignUp;
